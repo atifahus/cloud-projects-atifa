49f0b19947ec4f514ecac7f928dc57335fcca411
EC2 Default User <ec2-user@ip-172-31-45-6.ec2.internal>
2024-07-24 23:15:00 +0000

    installed package lab

--- shell-script-lab/installed-package.sh
+++ shell-script-lab/installed-package.sh
@@ -1,1 +1,10 @@
-clear
+#!/bin/bash
+
+# Define the package to check
+PACKAGE="git"
+
+if yum list installed | grep -q "$PACKAGE"; then
+    echo "The package $PACKAGE is installed."
+else
+    echo "The package $PACKAGE is not installed."
+fi
